2550707cb07065452408159fff140d9a30084a1b
owner <j13thompson@student.bridgew.edu>
2023-02-15 01:21:55 -0500

    Added a few basic starting blocks for GUI

--- GUI.py
+++ GUI.py
@@ -4,12 +4,38 @@
 # TODO: Ensure widgets are not editable
 
 import PyQt5
-from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtGui import QPalette
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
 
 cubes_app = QApplication([])
 
-label = QLabel('CUBES Database')
+# palette = QPalette()
+# palette.setColor(QPalette.ButtonText, Qt.red)
+# cubes_app.setPalette(palette)
+# cubes_app.setStyleSheet("QLabel { margin: 0.5ex; }")
+# cubes_app.setStyleSheet("QPushButton { margin: ex; }")
+#
+# label = QLabel('CUBES Database')
+#
+# layout = QVBoxLayout()
+# layout.addWidget(label)
+# layout.addWidget(QPushButton('Entry1'))
+# layout.addWidget(QPushButton('Entry2'))
 
-label.show()
+button = QPushButton('Click')
+
+
+def on_click_button():
+    alert = QMessageBox()
+    alert.setText("Clicked!")
+    alert.exec()
+
+
+button.click()
+button.show()
+# window = QWidget()
+# window.setLayout(layout)
+# window.show()
 
 cubes_app.exec()
